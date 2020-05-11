9ece3fed408214de02b3ea95ddc1557cb7bbf898
Brais Cidras <braisomary@gmail.com>
2020-05-11 12:42:06 +0200

    Drawer Menu management with custonm toolbar and toogle

--- app/src/main/java/com/sample/tabsanddrawermenu/MainActivity.kt
+++ app/src/main/java/com/sample/tabsanddrawermenu/MainActivity.kt
@@ -2,11 +2,35 @@ package com.sample.tabsanddrawermenu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import com.sample.tabsanddrawermenu.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.content_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var navigationView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Data binding
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        // Setting the toolbar
+        var toolbar = binding.contentMain.toolbar
+        setSupportActionBar(toolbar)
+
+        drawerLayout = binding.drawerLayout
+        navigationView = binding.navView
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.isDrawerIndicatorEnabled = true
+        toggle.syncState()
     }
 }
