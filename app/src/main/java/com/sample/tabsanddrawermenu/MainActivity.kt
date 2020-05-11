432dfd5eb375c77612c9c1cbd3d293d26412bfc3
Brais Cidras <braisomary@gmail.com>
2020-05-11 13:15:05 +0200

    Adding ViewPager TabLayout and TabItems to Main Activity

--- app/src/main/java/com/sample/tabsanddrawermenu/MainActivity.kt
+++ app/src/main/java/com/sample/tabsanddrawermenu/MainActivity.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.viewpager.widget.ViewPager
 import com.google.android.material.navigation.NavigationView
+import com.google.android.material.tabs.TabItem
+import com.google.android.material.tabs.TabLayout
 import com.sample.tabsanddrawermenu.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.content_main.view.*
 
@@ -14,6 +17,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var toggle: ActionBarDrawerToggle
     private lateinit var navigationView: NavigationView
+    private lateinit var viewPager: ViewPager
+    private lateinit var tabLayout: TabLayout
+    private lateinit var firstItem: TabItem
+    private lateinit var secondItem: TabItem
+    private lateinit var thirdItem: TabItem
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +33,14 @@ class MainActivity : AppCompatActivity() {
         var toolbar = binding.contentMain.toolbar
         setSupportActionBar(toolbar)
 
+        // Tab Layout
+        viewPager = binding.contentMain.view_pager
+        tabLayout = binding.contentMain.tab_layout
+        firstItem = binding.contentMain.first_item
+        secondItem = binding.contentMain.second_item
+        thirdItem = binding.contentMain.third_item
+
+        // Drawer Layout
         drawerLayout = binding.drawerLayout
         navigationView = binding.navView
 
